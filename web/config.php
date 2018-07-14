33fda4c56c24de3603b1a21f661cba11b025fafd
huxuesen <huxuesen@outlook.com>
2018-07-14 15:03:33 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -2,8 +2,8 @@
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyBfB_NO0Iwl-nW2yVlmlvb8mlse2CS36xk');//这里改成您的api key
 define('GJ_CODE', 'HK');//国家代码
-define('SITE_NAME', 'Youtube');//网站名字
-define('TITLENAME', 'Youtube');//网站名字
+define('SITE_NAME', 'You2');//网站名字
+define('TITLENAME', '视频');//网站名字
 define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
 define('EMAIL', 'huxuesen@outlook.com');//邮箱
 ?>
